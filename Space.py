e10e9dbc3c8ceee506fddcd2bb27f172654844bb
Randomizer <neoqhax@gmail.com>
2018-05-08 10:46:59 +0100

    Initial commit. First important conditional event

--- Space.py
+++ Space.py
@@ -1,15 +1,20 @@
 import time
 
 print("Welcome to space adventure say Yes to start")
+sword = "fists"
+shop1=''
 food = 0
 health = 10
+monster1 ="no"
 start = input("")
 if start == "Yes":
     print("You are going to space!!!! To the moon!!!!")
     money = 5
     print("You start your space adventure with 5 euros")
+    time.sleep(2)
     print("first you think to buy food.")
     food1 = input("Do you want to buy food?Yes or No?")
+    time.sleep(2)
     if food1 == "Yes":
         money = money - 2
         food = 2
@@ -39,6 +44,78 @@ if start == "Yes":
         print("You dont have food, you loose one of health")
         health = health - 1
         print("You have {} of health".format(health))
+    time.sleep(2)
+    print ("You arrived at the moon!!! you have {} of health and {} euros".format(health,money))
+    time.sleep(2)
+    print ("Oh NO YOU CRASHED!!!!")
+    time.sleep(2)
+    escape=input("Do you want to jump of and risk it? Yes or No?")
+    if escape=="Yes":
+        health= health - 4
+        print ("You lost 4 of health.Now you have {} of health".format(health))
+    elif escape == "No":
+        health = health - 7
+        print ("You have {} of health, you were lucky to survive".format(health))
+        print ("You hurt yourself during the crash you loose 2 of health")
+        health = health - 2
+        print ("You have {} of health".format(health))
+        if health <= 0:
+            print("Game over")
+            quit ()
+    print("You notice you rae not in the moon")
+    time.sleep(2)
+    print ("There are 2 coins on the floor")
+    collect1=input("Doy you want to pick them up?")
+    if collect1=="Yes" or collect1=="yes":
+        money = money + 2
+        monster1="yes"
+        print("you collected the two coins")
+        print("You see a beast running at you...")
+        time.sleep(2)
+        print("Now you see that you shouldent have picked the coins up")
+        print("there is a shop next to you")
+        time.sleep(2)
+        shop1=input("Do you want to buy one of the following?"
+                    "Wooden sword/ health pack/nothing? sword= 2 euros health pack = 1 euro you have {} euros. If you buy more that you can afford you die")
+        if shop1=="Wooden sword":
+            money= money-2
+            print("You payed 2 euros")
+            sword = "wooden sword"
+            if money <=1:
+                print ("Game over")
+                quit()
+        elif shop1=="health pack":
+            health=health + 6
+            if health >10:
+                health = 10
+                print("You have {} of health now".format(health))
+                money = money -1
+                if money <=0:
+                    print ("Game over")
+                    quit()
+        elif shop1=="Nothing":
+            print("You bought nothing")
+    if monster1=="yes":
+        print("The monster has got to you.")
+        time.sleep(2)
+        print ("You fight")
+        if sword=="wooden sword":
+            print("You took 5 of damage during the fight")
+            health=health-5
+            print("Now you have {} of health".format(health))
+            if health <=0:
+                print ("Game over")
+                quit ()
+        elif sword=="fists":
+            print("You lost 7 of health")
+            health = health-7
+            print("Now you have {} of health")
+            if health <=0:
+                 print ("Game over")
+                 quit()
+
+
+
 
 else:
     quit()
